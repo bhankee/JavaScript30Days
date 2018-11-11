ce0ca720686d0f537efe69da45d840d2a08ef597
Brad Hankee <brad.hankee@gmail.com>
2018-11-11 14:54:34 -0500

    Completed Day 23 Speech Synthesis.

--- Day 1 - Drum/drum.js
+++ Day 1 - Drum/drum.js
@@ -1,11 +1,11 @@
-window.addEventListener('keydown',function(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    console.log(audio);
-    if(!audio)return; //Stops if no audio
-    audio.currentTime = 0 //Resets time to zero
-    audio.play();
-    key.classList.add('playing');
+window.addEventListener('keydown', function(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  console.log(audio);
+  if (!audio) return; //Stops if no audio
+  audio.currentTime = 0; //Resets time to zero
+  audio.play();
+  key.classList.add('playing');
 });
 
 const keys = document.querySelectorAll('.key');
